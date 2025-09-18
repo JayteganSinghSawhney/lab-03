62bf23d3860d19c563bc20ef45081da0651fb72e
JT-Singh7 <jaytegansinghs@gmail.com>
2025-09-18 16:08:54 -0600

    Implemented Lab3 Requirements

---
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,85 @@
+package com.example.listycitylab3;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+    interface EditCityDialogListener {
+        void editCity(City city);
+    }
+    private EditCityDialogListener listener;
+    private City city;
+    private int position;
+
+    public static EditCityFragment newInstance(City city, int position) {
+        Bundle args = new Bundle();
+        args.putSerializable("city", city);
+        args.putInt("position", position);
+        EditCityFragment fragment = new EditCityFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            city = (City) args.getSerializable("city");
+            position = args.getInt("position");
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof EditCityDialogListener) {
+            listener = (EditCityDialogListener) context;
+            } else {
+            throw new RuntimeException(context + "must implement EditCityDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view =
+                LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+
+        //Do something
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+
+        if (city != null) {
+            editCityName.setText(city.getName());
+            editProvinceName.setText(city.getProvince());
+        }
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Edit a city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Edit", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+                    if (city != null) {
+                        city.setName(cityName);
+                        city.setProvince(provinceName);
+                        listener.editCity(city);
+                    }
+                })
+                .create();
+                }
+
+}
